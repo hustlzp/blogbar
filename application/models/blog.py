9d416e3851af3a37595e06bfe6ec92bcfe0f5d13
hustlzp <hustlzp@qq.com>
2014-11-02 11:18:10 +0800

    Add Post model.

--- application/models/blog.py
+++ application/models/blog.py
@@ -14,3 +14,16 @@ class Blog(db.Model):
 
     def __repr__(self):
         return '<Blog %s>' % self.name
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(500))
+    title = db.Column(db.String(200))
+    content = db.Column(db.Text)
+    pub_at = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))
+    blog = db.relationship('Blog', backref=db.backref('quotes', lazy='dynamic',
+                                                      order_by='desc(Post.pub_at)'))
