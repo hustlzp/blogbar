2a71a6432f5b1e8980191a59b5907576d2e25d80
hustlzp <hustlzp@qq.com>
2014-11-07 09:53:21 +0800

    Add blog.view page.

--- application/controllers/blog.py
+++ application/controllers/blog.py
@@ -7,6 +7,12 @@ from ..utils.blog import grab_blog
 bp = Blueprint('blog', __name__)
 
 
+@bp.route('<int:uid>')
+def view(uid):
+    blog = Blog.query.get_or_404(uid)
+    return render_template('blog/view.html', blog=blog)
+
+
 @bp.route('/add', methods=['GET', 'POST'])
 def add():
     """添加博客"""
