2eca4cc2ea2175fe4ba6920820616c1c51ffb11b
hustlzp <hustlzp@qq.com>
2014-11-07 09:40:40 +0800

    When publisted or updated changes, update post.

--- application/utils/blog.py
+++ application/utils/blog.py
@@ -20,34 +20,49 @@ def grab_blog(blog):
     db.session.add(blog)
     print(blog.title)
 
-    # 最新博文
     for entry in result.entries:
         identity = entry.id if 'id' in entry else entry.link
         post = blog.posts.filter(Post.unique_id == identity).first()
+        # 新博文
         if not post:
-            post = Post(title=entry.title, url=entry.link, unique_id=identity)
-            if 'updated_parsed' in entry:
-                post.updated_at = _get_time(entry.updated_parsed)
-            if 'published_parsed' in entry:
-                post.published_at = _get_time(entry.published_parsed)
-
-            if 'content' in entry:
-                if isinstance(entry.content, list):
-                    post.content = entry.content[0].value
-                else:
-                    post.content = entry.content
-            elif 'summary' in entry:
-                post.content = entry.summary
+            post = Post(unique_id=identity)
+            _get_info_to_post(post, entry)
             blog.posts.append(post)
             print(" new - %s" % post.title)
         else:
+            # 更新
+            updated_at = None
+            published_at = None
+
             if 'updated_parsed' in entry:
-                post.updated_at = _get_time(entry.updated_parsed)
+                updated_at = _get_time(entry.updated_parsed)
             if 'published_parsed' in entry:
-                post.published_at = _get_time(entry.published_parsed)
-            print(" update - %s" % post.title)
-            db.session.add(post)
+                published_at = _get_time(entry.published_parsed)
+
+            if (updated_at and updated_at != post.updated_at) or (
+                        published_at and published_at != post.published_at):
+                _get_info_to_post(post)
+                print(" update - %s" % post.title)
+                db.session.add(post)
     db.session.commit()
+
+
+def _get_info_to_post(post, entry):
+    """将entry中的信息转存到post中"""
+    post.title = entry.title
+    post.url = entry.link
+    if 'updated_parsed' in entry:
+        post.updated_at = _get_time(entry.updated_parsed)
+    if 'published_parsed' in entry:
+        post.published_at = _get_time(entry.published_parsed)
+
+    if 'content' in entry:
+        if isinstance(entry.content, list):
+            post.content = entry.content[0].value
+        else:
+            post.content = entry.content
+    elif 'summary' in entry:
+        post.content = entry.summary
 
 
 def _get_time(time_struct):
