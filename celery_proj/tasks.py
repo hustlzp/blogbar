f9a5aa714ea6e3da03ed9e896b0b5bf5b605c940
hustlzp <hustlzp@qq.com>
2014-11-08 16:42:15 +0800

    Update tasks.

--- celery_proj/tasks.py
+++ celery_proj/tasks.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from __future__ import absolute_import
+from fabric.api import run, env, cd, prefix, shell_env
 from celery_proj.app import app
 from application import create_app
 from application.models import db, Blog
@@ -9,17 +10,18 @@ from application.utils.blog import grab_blog
 @app.task
 def grab():
     """获取最新feed数据"""
-    flask_app = create_app()
-    with flask_app.app_context():
-        for blog in Blog.query:
-            try:
-                grab_blog(blog)
-            except Exception, e:
-                blog.last_status = False
-                print ("Failed - %s" % blog.title)
-                print e
-            else:
-                blog.last_status = True
-                print ("Success - %s" % blog.title)
-            db.session.add(blog)
-            db.session.commit()
+    with shell_env(MODE='PRODUCTION'):
+        flask_app = create_app()
+        with flask_app.app_context():
+            for blog in Blog.query:
+                try:
+                    grab_blog(blog)
+                except Exception, e:
+                    blog.last_status = False
+                    print ("Failed - %s" % blog.title)
+                    print e
+                else:
+                    blog.last_status = True
+                    print ("Success - %s" % blog.title)
+                db.session.add(blog)
+                db.session.commit()
