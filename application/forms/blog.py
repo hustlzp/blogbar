e2d18a9b51001a8a172b91f5d06a01253b61b454
hustlzp <hustlzp@qq.com>
2014-11-13 20:51:06 +0800

    Allow feed to be empty

--- application/forms/blog.py
+++ application/forms/blog.py
@@ -12,7 +12,4 @@ class BlogForm(Form):
     ])
     title = StringField('标题', [DataRequired('不能为空')])
     author = StringField('作者', [DataRequired('不能为空')])
-    feed = StringField('RSS', [
-        DataRequired('不能为空'),
-        URL('格式错误')
-    ])
+    feed = StringField('RSS')
