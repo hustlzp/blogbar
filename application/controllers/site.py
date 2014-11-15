426c15cc2d8222d35defbe6c8f867297b5712512
hustlzp <hustlzp@qq.com>
2014-11-15 19:08:48 +0800

    Update index page.

--- application/controllers/site.py
+++ application/controllers/site.py
@@ -10,7 +10,9 @@ def index():
     """首页"""
     blogs = Blog.query
     latest_posts = Post.query.order_by(Post.created_at.desc()).limit(10)
-    return render_template('site/index.html', blogs=blogs, latest_posts=latest_posts)
+    latest_blogs = Blog.query.order_by(Blog.created_at.desc()).limit(10)
+    return render_template('site/index.html', blogs=blogs, latest_posts=latest_posts,
+                           latest_blogs=latest_blogs)
 
 
 @bp.route('/about')
