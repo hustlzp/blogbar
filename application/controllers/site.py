d5bf3db1053b261e6d1618e46aabb49c0a18f0a1
hustlzp <hustlzp@qq.com>
2014-11-15 20:44:48 +0800

    Order by published_at firstly, updated_at secondly.

--- application/controllers/site.py
+++ application/controllers/site.py
@@ -11,7 +11,7 @@ def index():
     blogs = Blog.query.filter(Blog.is_approved)
     blogs_count = blogs.count()
     posts_count = Post.query.count()
-    latest_posts = Post.query.order_by(Post.created_at.desc()).limit(10)
+    latest_posts = Post.query.order_by(Post.published_at.desc(), Post.updated_at.desc()).limit(10)
     latest_blogs = Blog.query.filter(Blog.is_approved).order_by(Blog.created_at.desc()).limit(10)
     return render_template('site/index.html', blogs=blogs, latest_posts=latest_posts,
                            latest_blogs=latest_blogs, blogs_count=blogs_count,
