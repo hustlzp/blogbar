7b97f6ffa1a6882d05a87a0c4283c4d2d4f2fe3f
hustlzp <hustlzp@qq.com>
2014-11-15 20:18:03 +0800

    Show blogs count and posts count in index page.

--- application/controllers/site.py
+++ application/controllers/site.py
@@ -9,10 +9,13 @@ bp = Blueprint('site', __name__)
 def index():
     """首页"""
     blogs = Blog.query.filter(Blog.is_approved)
+    blogs_count = blogs.count()
+    posts_count = Post.query.count()
     latest_posts = Post.query.order_by(Post.created_at.desc()).limit(10)
     latest_blogs = Blog.query.filter(Blog.is_approved).order_by(Blog.created_at.desc()).limit(10)
     return render_template('site/index.html', blogs=blogs, latest_posts=latest_posts,
-                           latest_blogs=latest_blogs)
+                           latest_blogs=latest_blogs, blogs_count=blogs_count,
+                           posts_count=posts_count)
 
 
 @bp.route('/about')
