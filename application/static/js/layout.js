9a9e49ea610949cb8d5872307c9064cf04788c51
hustlzp <hustlzp@qq.com>
2014-11-18 13:19:41 +0800

    Convert utc time to localtime.

--- application/static/js/layout.js
+++ application/static/js/layout.js
@@ -4,6 +4,15 @@ $(function () {
         setTimeout(showFlash, 200);
         setTimeout(hideFlash, 2000);
     });
+
+    // 转换成UTC时间
+    $('.utc-time').each(function () {
+        var time = $(this).text();
+        var date = new Date();
+        var offset = date.getTimezoneOffset();
+        time = moment(time, "YYYY-MM-DD HH:mm:ss").subtract(offset, 'minutes');
+        $(this).text(time.format("YYYY-MM-DD HH:mm:ss"));
+    });
 });
 
 /**
