abf3f209bc1f3729fb53a1c2566006fffb2cc57e
hustlzp <hustlzp@qq.com>
2014-11-22 16:36:02 +0800

    Hide posts whose blog is not approved.

--- application/controllers/blog.py
+++ application/controllers/blog.py
@@ -76,7 +76,8 @@ def feed(uid):
 @bp.route('/posts', defaults={'page': 1})
 @bp.route('/posts/page/<int:page>')
 def posts(page):
-    posts = Post.query.filter(~Post.is_duplicate).order_by(
-        Post.published_at.desc(),
-        Post.updated_at.desc()).paginate(page, 20)
+    posts = Post.query. \
+        filter(~Post.is_duplicate).filter(Post.blog.has(Blog.is_approved)). \
+        order_by(Post.published_at.desc(), Post.updated_at.desc()). \
+        paginate(page, 20)
     return render_template('blog/posts.html', posts=posts)
