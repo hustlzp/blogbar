214071c5fb6e060aec6bb25fc267782564d85df3
hustlzp <hustlzp@qq.com>
2014-11-27 20:06:59 +0800

    Update style for blog.add

--- application/forms/blog.py
+++ application/forms/blog.py
@@ -25,12 +25,12 @@ class AddBlogForm(Form):
         DataRequired('不能为空'),
         check_url,
         URL('格式错误')
-    ])
-    title = StringField('标题', [DataRequired('不能为空')])
-    subtitle = StringField('副标题', description='选填')
-    author = StringField('作者', [DataRequired('不能为空')])
-    feed = StringField('Feed', [check_url], description='选填')
-    since = StringField('Since', description='选填，博客从哪一年开始写的，如2012')
+    ], description='博客 URL 地址')
+    title = StringField('标题', [DataRequired('不能为空')], description='标题')
+    subtitle = StringField('副标题', description='副标题  /  选填')
+    author = StringField('作者', [DataRequired('不能为空')], description='博主')
+    feed = StringField('Feed', [check_url], description='RSS 订阅地址  / 选填')
+    since = StringField('Since', description='博客从哪一年开始写的，如 2012  /  选填')
 
     def validate_url(self, field):
         blog = Blog.query.filter(Blog.url == field.data).first()
