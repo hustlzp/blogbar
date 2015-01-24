56bf50151d7001c0d47db252e56d839343869ab8
hustlzp <hustlzp@qq.com>
2015-01-24 20:59:38 +0800

    Add password hash.

--- application/forms/account.py
+++ application/forms/account.py
@@ -9,26 +9,25 @@ class SigninForm(Form):
     """Form for signin"""
     email = StringField('邮箱',
                         validators=[
-                            DataRequired(),
-                            Email()
+                            DataRequired('邮箱不能为空'),
+                            Email('邮箱格式错误')
                         ],
-                        description='Email')
+                        description='邮箱')
 
     password = PasswordField('密码',
-                             validators=[DataRequired()],
-                             description='Password')
+                             validators=[DataRequired('密码不能为空')],
+                             description='密码')
 
     def validate_email(self, field):
         user = User.query.filter(User.email == self.email.data).first()
         if not user:
-            raise ValueError("Account doesn't exist.")
+            raise ValueError("账户不存在")
 
     def validate_password(self, field):
         if self.email.data:
-            user = User.query.filter(User.email == self.email.data,
-                                     User.password == self.password.data).first()
-            if not user:
-                raise ValueError('Password cannot match the Email.')
+            user = User.query.filter(User.email == self.email.data).first()
+            if not user or not user.check_password(self.password.data):
+                raise ValueError('密码错误')
             else:
                 self.user = user
 
--- application/models/user.py
+++ application/models/user.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import datetime
 from ._base import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -11,6 +12,16 @@ class User(db.Model):
     password = db.Column(db.String(200))
     is_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __setattr__(self, name, value):
+        # 每当设置password时，自动进行hash
+        if name == 'password':
+            value = generate_password_hash(value)
+        super(User, self).__setattr__(name, value)
+
+    def check_password(self, password):
+        """验证密码"""
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return '<User %s>' % self.name
