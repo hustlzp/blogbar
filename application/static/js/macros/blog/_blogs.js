9f73d8e8773a8ed4b1329b719393f6c3977e67d8
hustlzp <hustlzp@qq.com>
2015-09-21 01:12:48 +0800

    Refactor macro file structure.

---
+++ application/static/js/macros/blog/_blogs.js
@@ -0,0 +1,23 @@
+$('.btn-wap').click(function () {
+    var blogId = parseInt($(this).data('blog-id')),
+        url = "",
+        btnWap = $(this);
+
+    if ($(this).hasClass('unsubscribe')) {
+        url = "{{ url_for('blog.unsubscribe') }}";
+    } else {
+        url = "{{ url_for('blog.subscribe') }}";
+    }
+
+    $.ajax({
+        url: url,
+        data: {
+            blog_id: blogId
+        },
+        method: 'POST',
+        dataType: 'json',
+        success: function () {
+            btnWap.toggleClass('unsubscribe');
+        }
+    });
+});
