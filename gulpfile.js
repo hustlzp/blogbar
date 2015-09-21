1bf5edab5479a0a48fd1a17391832249361694ad
hustlzp <hustlzp@qq.com>
2015-09-21 12:22:28 +0800

    Finish refactor.

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,42 @@ var less = require('gulp-less');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var gulpif = require('gulp-if');
 
 var cssRoot = './application/static/css';
 var jsRoot = './application/static/js';
 
 gulp.task('macros-css', function () {
-    var stream = gulp.src(path.join(cssRoot, '**/_*.less'))
+    return gulp
+        .src(path.join(cssRoot, '**/_*.less'))
+        .pipe(plumber())
         .pipe(less({
             paths: [cssRoot]
         }))
-        .pipe(concat('macros.css'));
-
-    if (process.env.MODE === 'PRODUCTION') {
-        stream = stream.pipe(minifyCss({compatibility: 'ie8'}));
-    }
-
-    return stream.pipe(gulp.dest('./output/'));
+        .pipe(concat('macros.css'))
+        .pipe(gulpif(inProduction, minifyCss({compatibility: 'ie8'})))
+        .pipe(gulp.dest('./application/static/output/'));
 });
 
 gulp.task('macros-js', function () {
-    var stream = gulp.src(path.join(jsRoot, '**/_*.js'))
-        .pipe(concat('macros.js'));
-
-    if (process.env.MODE === 'PRODUCTION') {
-        stream = stream.pipe(uglify());
-    }
-
-    return stream.pipe(gulp.dest('./output/'));
+    return gulp
+        .src(path.join(jsRoot, '**/_*.js'))
+        .pipe(plumber())
+        .pipe(concat('macros.js'))
+        .pipe(gulpif(inProduction, uglify()))
+        .pipe(gulp.dest('./application/static/output/'));
 });
 
 gulp.task('default', ['macros-css', 'macros-js'], function () {
 });
+
+gulp.task('watch', function () {
+    gulp.watch(path.join(jsRoot, '**/_*.js'), ['macros-js']);
+    gulp.watch(path.join(cssRoot, '**/_*.less'), ['macros-css']);
+});
+
+function inProduction() {
+    return process.env.MODE === 'PRODUCTION';
+}
